ec617d799ce71874b8fa03ef8edb94f40d4d4abc
Danil Sch <73740737+BreakingTV@users.noreply.github.com>
2021-03-17 17:37:36 +0100

    Improved the Name/URL function!

--- Downloader.py
+++ Downloader.py
@@ -4,16 +4,27 @@ import pytube
 import urllib.request
 import re
 
-search_keyword = input("Name/URL: ").replace(" ", "+")
-html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-result = "https://www.youtube.com/watch?v=" + video_ids[0]
+search_keyword = input("Name/URL: ").replace(" ", "+").lower()
 
-youtube_video = pytube.YouTube(result)
-stream = youtube_video.streams.get_highest_resolution()
+if search_keyword.startswith("https://"):
+    youtube_video = pytube.YouTube(search_keyword)
+    stream = youtube_video.streams.get_highest_resolution()
 
-stream.download()
+    stream.download()
 
-print("Download Successful")
+    print("Download Successful")
 
-shutil.move(stream.title + ".mp4", input("Where you want to store this video? Full path: ") + "/")
+    shutil.move(stream.title + ".mp4", input("Where you want to store this video? Full path: ") + "/")
+else:
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    result = "https://www.youtube.com/watch?v=" + video_ids[0]
+
+    youtube_video = pytube.YouTube(result)
+    stream = youtube_video.streams.get_highest_resolution()
+
+    stream.download()
+
+    print("Download Successful")
+
+    shutil.move(stream.default_filename, input("Where you want to store this video? Full path: ") + "/")
