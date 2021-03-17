857db4315a90f663314275bef41b71786284915f
Danil Sch <73740737+BreakingTV@users.noreply.github.com>
2021-03-17 17:19:34 +0100

    Added in search spaces

--- Downloader.py
+++ Downloader.py
@@ -4,7 +4,7 @@ import pytube
 import urllib.request
 import re
 
-search_keyword = input("Name/URL: ")
+search_keyword = input("Name/URL: ").replace(" ", "+")
 html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
 video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 result = "https://www.youtube.com/watch?v=" + video_ids[0]
