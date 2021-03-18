69914bd2faff40373709710df243cf95dea25584
Danil Sch <73740737+BreakingTV@users.noreply.github.com>
2021-03-18 15:53:20 +0100

    Improved the code and the name code

--- Downloader.py
+++ Downloader.py
@@ -1,54 +1,58 @@
 import shutil
 import pytube
-
+import sys
 import urllib.request
 import re
+
+videos = [0, 1, 2, 3]
 
 search_keyword = input("Name/URL: ").replace(" ", "+").lower()
 
 if search_keyword.startswith("https://"):
+    print("Their can be some errors with the get https://")
     youtube_video = pytube.YouTube(search_keyword)
-    stream = youtube_video.streams.get_highest_resolution()
+    stream = youtube_video.streams.filter(res="1080p").first()
+    print("Using: " + stream.title)
+    print("Loading...")
 
     stream.download()
-
     print("Download Successful")
+    print("when you don't know press Enter")
+    inp = input("Where you want to store this video? Full path: ")
+    if inp.startswith(""):
+        sys.exit()
+    else:
+        shutil.move(stream.default_filename, inp + "/")
 
-    shutil.move(stream.title + ".mp4", input("Where you want to store this video? Full path: ") + "/")
 else:
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 
     print("Loading...")
     print(" ")
-    print("[1]" + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[0]).title + " - " + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[0]).author)
-    print("[2]" + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[1]).title + " - " + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[1]).author)
-    print("[3]" + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[2]).title + " - " + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[2]).author)
-    print("[4]" + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[3]).title + " - " + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[3]).author)
-    print("[5]" + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[4]).title + " - " + pytube.YouTube("https://www.youtube.com/watch?v=" + video_ids[4]).author)
+    for v in videos:
+        print("[" + str(v) + "]" + pytube.YouTube(
+            "https://www.youtube.com/watch?v=" + video_ids[v]).title + " - " + pytube.YouTube(
+            "https://www.youtube.com/watch?v=" + video_ids[v]).author)
     print(" ")
     print("Choose one video you want to download")
 
 inp = input()
-
-if inp == "1":
-    result = "https://www.youtube.com/watch?v=" + video_ids[0]
-
-if inp == "2":
-    result = "https://www.youtube.com/watch?v=" + video_ids[1]
-
-if inp == "3":
-    result = "https://www.youtube.com/watch?v=" + video_ids[2]
-
-if inp == "4":
-    result = "https://www.youtube.com/watch?v=" + video_ids[3]
-
-if inp == "5":
-    result = "https://www.youtube.com/watch?v=" + video_ids[4]
+inp = int(inp)
+result = "https://www.youtube.com/watch?v=" + video_ids[inp]
 
 youtube_video = pytube.YouTube(result)
-stream = youtube_video.streams.get_highest_resolution()
-stream.download()
-print("Download Successful")
-shutil.move(stream.default_filename, input("Where you want to store this video? Full path: ") + "/")
+print("Using: " + pytube.YouTube(result).title)
+print("Loading...")
 
+stream = youtube_video.streams.filter(res="1080p").first()
+
+stream.download()
+
+print("Download Successful")
+print("when you don't know press Enter")
+inp = input("Where you want to store this video? Full path: ")
+if inp.startswith(""):
+    sys.exit()
+else:
+    shutil.move(stream.default_filename, inp + "/")
