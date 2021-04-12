cf0931f0a5bf589e10923a865ff0a99c1ba347c2
Danil Sch <73740737+BreakingTV@users.noreply.github.com>
2021-04-12 08:53:08 +0200

    Added playlist & Options

--- Downloader.py
+++ Downloader.py
@@ -1,33 +1,16 @@
-import shutil
-import pytube
-import sys
-import urllib.request
 import re
-import os
+import shutil
+import urllib.request
+
+import pytube
 from moviepy.editor import *
-import moviepy.editor as mpe
 
-videos = [0]
+videos = [0, 1, 2, 3, 4, 5]
 
-search_keyword = input("Name/URL: ").replace(" ", "+").lower()
+Option = input("option:").lower()
 
-if search_keyword.startswith("https://"):
-    print("Their can be some errors with the get https://")
-    youtube_video = pytube.YouTube(search_keyword)
-    stream = youtube_video.streams.filter(res="1080p").first()
-    print("Using: " + stream.title)
-    print("Loading...")
-
-    stream.download()
-    print("Download Successful")
-    print("when you don't know press Enter")
-    inp = input("Where you want to store this video? Full path: ")
-    if inp.startswith(""):
-        sys.exit()
-    else:
-        shutil.move(stream.default_filename, inp + "/")
-
-else:
+if Option == "video".lower():
+    search_keyword = input("Name: ").replace(" ", "+").lower()
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
     video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 
@@ -40,33 +23,66 @@ else:
     print(" ")
     print("Choose one video you want to download")
 
-inp = input()
-inp = int(inp)
-result = "https://www.youtube.com/watch?v=" + video_ids[inp]
+    inp = input()
+    inp = int(inp)
+    result = "https://www.youtube.com/watch?v=" + video_ids[inp]
 
-youtube_video = pytube.YouTube(result)
-print("Using: " + pytube.YouTube(result).title)
+    youtube_video = pytube.YouTube(result)
+    print("Using: " + pytube.YouTube(result).title)
 
-stream = youtube_video.streams.filter(res="1080p").first()
+    stream = youtube_video.streams.filter(res="720p").first()
 
-print("when you don't know press Enter")
-inp = input("Where you want to store this video? Full path: ")
+    print("when you don't know press Enter")
+    inp = input("Where you want to store this video? Full path: ")
 
-if inp.startswith(""):
-    print("Using file location")
-else:
-    os.chdir(inp)
+    if inp.startswith(""):
+        print("Using file location")
+    else:
+        os.chdir(inp)
     print("Location is: " + inp + "/")
 
-stream.download()
+    stream.download()
 
-# stream_audio = youtube_video.streams.get_audio_only()
-# stream_audio.download("", "audio")
+    # stream_audio = youtube_video.streams.get_audio_only()
+    # stream_audio.download("", "audio")
 
-# audio = mpe.AudioFileClip("audio.mp4")
-# video1 = mpe.VideoFileClip(stream.default_filename)
-# final = video1.set_audio(audio)
-# final.write_videofile("/home/danil/Videos/output.mp4", codec='mp4', audio_codec='libvorbis')
+    # audio = mpe.AudioFileClip("audio.mp4")
+    # video1 = mpe.VideoFileClip(stream.default_filename)
+    # final = video1.set_audio(audio)
+    # final.write_videofile("/home/danil/Videos/output.mp4", codec='mp4', audio_codec='libvorbis')
 
-print("Download Successful")
+    print("Download Successful")
 
+else:
+    if Option == "https".lower():
+        search_keyword = input("URL: ").replace(" ", "+").lower()
+        print("Their can be some errors with the get https://")
+        youtube_video = pytube.YouTube(search_keyword)
+        stream = youtube_video.streams.filter(res="1080p").first()
+        print("Using: " + stream.title)
+        print("Loading...")
+
+        stream.download()
+        print("Download Successful")
+        print("when you don't know press Enter")
+        inp = input("Where you want to store this video? Full path: ")
+        if inp.startswith(""):
+            sys.exit()
+        else:
+            shutil.move(stream.default_filename, inp + "/")
+    else:
+        if Option == "playlist".lower():
+            YP = pytube.Playlist(input("URL: "))
+
+            os.mkdir(YP.title)
+            os.chdir(YP.title)
+
+            for v in YP.video_urls:
+                video = pytube.YouTube(v)
+
+                stream = video.streams.filter(res="720p").first()
+                print("Downloading: " + stream.title)
+                stream.download()
+                print("Downloaded " + stream.title)
+
+            print("All Videos Downloaded!")
